d58b0275c5e9e802bf43d859f8617afce06b8afd
Great <okemakindetobi@gmail.com>
2023-01-07 19:26:03 +0100

    Update

--- OrganicConsumption/Assets/Scripts/Planting Scripts/PlantSO.cs
+++ OrganicConsumption/Assets/Scripts/Planting Scripts/PlantSO.cs
@@ -6,6 +6,9 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Plant", menuName = "PlantSO")]
 public class PlantSO : ScriptableObject
 {
+    public enum  PlantVariety { Banana, }
+    public PlantVariety plantVariety; 
+
     public Sprite[] plantGrowthStages = new Sprite[4];
     // This is time it takes for the plant to move from the first stage to the last
     public float growthTime = 5f; 
