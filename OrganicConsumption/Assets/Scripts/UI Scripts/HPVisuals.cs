3bb9b47b50aa22a405cf37640929925cc962ee5a
TerranMJDMO <121522820+TerranMJDMO@users.noreply.github.com>
2023-01-08 23:27:02 -0500

    Cleaned up Art assets in Resources
    Deleted old placeholder art no longer in use
    Imported more art assets

---
+++ OrganicConsumption/Assets/Scripts/UI Scripts/HPVisuals.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class HPVisuals : MonoBehaviour
+{
+    [SerializeField] Sprite HPFull, HP2, HP1, HP0;
+    Image graphic;
+    int oldHealth = 3;
+
+    private void Awake() => graphic = transform.GetChild(0).GetComponent<Image>(); 
+
+    public void UpdateHealthVisual(int currentHealth)
+    {
+        if (oldHealth > currentHealth)
+        {
+            transform.DOPunchPosition(new Vector3(10, 0, 0), 1);
+        }
+        oldHealth = currentHealth;
+
+        switch (currentHealth)
+        {
+            case 3:
+                graphic.sprite = HPFull;
+                break;
+            case 2:
+                graphic.sprite = HP2;
+                break;
+            case 1:
+                graphic.sprite = HP1;
+                break;
+            case 0:
+                graphic.sprite = HP0;
+                break;
+        }
+    }
+}
