8247a44df3f2d2fb038f21da70e03707ad17fac9
TerranMJDMO <121522820+TerranMJDMO@users.noreply.github.com>
2023-01-09 13:44:26 -0500

    UI CHanges

--- OrganicConsumption/Assets/Scripts/UI Scripts/ButtonVisualUpdater.cs
+++ OrganicConsumption/Assets/Scripts/UI Scripts/ButtonVisualUpdater.cs
@@ -1,28 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.EventSystems;
 using TMPro;
 
 public class ButtonVisualUpdater : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-    TextMeshPro name;
-    TextMeshPro cost;
+    [SerializeField] TextMeshProUGUI textDesc;
+    [SerializeField] Image itemImage;
+
+    [Space, SerializeField] string mealDescription;
+    [SerializeField] Sprite mealSprite;
+
+    TextMeshProUGUI mealName;
+    TextMeshProUGUI cost;
+    Image scripIcon;
+    Image background;
+    [SerializeField] Color hoverColor;
+
 
     private void Awake()
     {
-        transform.GetChild(0).GetComponent<TextMeshPro>();
-        //transform.GetChild(1).GetComponent<>();
-        transform.GetChild(2).GetComponent<TextMeshPro>();
+        background = transform.GetChild(0).GetComponent<Image>();
+        mealName = transform.GetChild(1).GetComponent<TextMeshProUGUI>();
+        scripIcon = transform.GetChild(2).GetComponent<Image>();
+        cost = transform.GetChild(3).GetComponent<TextMeshProUGUI>();
+
+        itemImage.sprite = mealSprite;
+        textDesc.text = mealDescription;
     }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        Debug.Log("Entered");
+        background.color = hoverColor;
+        mealName.color = Color.black;
+        cost.color = Color.black;
+        scripIcon.color = Color.black;
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        Debug.Log("Exited");
+        background.color = Color.black;
+        mealName.color = Color.white;
+        cost.color = Color.white;
+        scripIcon.color = Color.white;
     }
 }
